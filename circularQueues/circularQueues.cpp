f4a34bf4def19af041639abf01ef2baff185f4dc
Monosix8 <162055149+Monosix8@users.noreply.github.com>
2024-05-30 19:06:39 +0700

    void display

--- circularQueues/circularQueues.cpp
+++ circularQueues/circularQueues.cpp
@@ -51,5 +51,17 @@ public:
 			FRONT = -1;
 			REAR = -1;
 		}
-		
+		else {
+			// jika element yang dihapus berada di posisi terakhir array, kembali ke awal array
+			if (FRONT == max - 1)
+				FRONT = 0;
+			else
+				FRONT = FRONT + 1;
+		}
+	}
 
+	void dsiplay() {
+		int FRONT_position = FRONT;
+		int REAR_position = REAR;
+		
+		
