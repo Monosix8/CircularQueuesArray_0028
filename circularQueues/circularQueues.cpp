5c0b2178d90a12813d602d82234febfefc41d417
Monosix8 <162055149+Monosix8@users.noreply.github.com>
2024-05-30 19:09:33 +0700

    case

--- circularQueues/circularQueues.cpp
+++ circularQueues/circularQueues.cpp
@@ -81,4 +81,51 @@ public:
 			}
 			cout << endl;
 		}
-		
+		else {
+			// jika FRONT_position > REAR_position, iterasi dari FRONT hingga akhir array
+			while (FRONT_position <= max - 1) {
+				cout << queue_array[FRONT_position] << "  ";
+				FRONT_position++;
+			}
+
+			FRONT_position = 0;
+
+			// Iterasi dari awal array hingga REAR
+			while (FRONT_position <= REAR_position) {
+				cout << queue_array[FRONT_position] << "  ";
+				FRONT_position++;
+			}
+			cout << endl;
+		}
+	}
+};
+
+int main() {
+	Queues q;
+	char ch;
+
+	while (true) {
+		try {
+			cout << "Menu" << endl;
+			cout << "1. Implement insert operation" << endl;
+			cout << "2. Implement delete operation" << endl;
+			cout << "3. Display values" << endl;
+			cout << "4. Exit" << endl;
+			cout << "Enter your choice (1-4): ";
+			cin >> ch;
+			cout << endl;
+
+			switch (ch) {
+			case '1': {
+
+				q.insert();
+				break;
+			}
+			case '2': {
+				q.remove();
+				break;
+			}
+		}
+	}
+};
+
