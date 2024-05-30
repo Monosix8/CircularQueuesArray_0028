8b4c284e3b64367d999a64e06270c2e52a974c40
Monosix8 <162055149+Monosix8@users.noreply.github.com>
2024-05-30 19:06:18 +0700

    rear

--- circularQueues/circularQueues.cpp
+++ circularQueues/circularQueues.cpp
@@ -46,4 +46,10 @@ public:
 		}
 		cout << "\nThe element deleted from the queue is: " << queue_array[FRONT] << "\n";
 
+		// Cek jika antrian hanya memiliki satu element
+		if (FRONT == REAR) {
+			FRONT = -1;
+			REAR = -1;
+		}
 		
+
